25d1db1a56f93d31e0a02d2137fded412ccf67b4
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2012-06-22 19:27:18 +0200

    dodanie testów - ładowanie pluginów, funkcja rozpoznawnia rodzajów napisów (recognize)

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-
+#
 #       test
 #       
 #       Copyright 2012 Daniel Dereziński <daniel.derezinski@gmial.com>
@@ -20,3 +20,115 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA
 
+import os
+from gkcore.core import pluginLoad, pluginInstance
+from gkcore.convert import ConvertBase
+import unittest
+import inspect
+
+
+
+class PluginLoadTest(unittest.TestCase):
+    def setUp(self):
+        plugindir1 = os.getcwd() + '/plugins'
+        self.plugins_dir = [plugindir1]
+    
+class ConvertPluginLoadTest(PluginLoadTest):
+    def setUp(self):
+        PluginLoadTest.setUp(self)
+        self.plugin_subclass_convert = ConvertBase
+    def test_loadConvertPlugins(self):
+        loaded_plugins = pluginLoad(self.plugins_dir, self.plugin_subclass_convert)
+        self.assertNotEqual(len(loaded_plugins), 0, "Plugins not Loaded")
+    def test_doInstanceConvertPlugins(self):
+        loaded_plugins_classes = pluginLoad(self.plugins_dir, self.plugin_subclass_convert)
+        loaded_plugins_instances = []
+        for plc in loaded_plugins_classes:
+            plugin_instance = plc()
+            loaded_plugins_instances.append(plugin_instance)
+        self.assertNotEqual(len(loaded_plugins_instances), 0, "No Plugin Instance")
+        
+class ConvertPluginRcognizeTest(PluginLoadTest):
+    def setUp(self):
+        PluginLoadTest.setUp(self)
+        # load and do instances of plugns
+        plugins = pluginLoad(self.plugins_dir, ConvertBase)
+        self.plugins = pluginInstance(plugins)
+        # subtitles files
+        self.test_files_dir = os.getcwd() + '/test_files/'
+        self.sub_mdvd = self.test_files_dir + 'mdvd.txt'
+        self.sub_mpl2 = self.test_files_dir + 'mpl2.txt'
+        self.sub_srt = self.test_files_dir + 'srt.srt'
+        self.sub_tmpl = self.test_files_dir + 'tmpl.txt'
+        
+    def test_mdvdRecognize(self):
+        given_subs_type = 'mdvd'
+        sub_file = self.sub_mdvd
+        
+        subs_type = []
+        
+        for pli in self.plugins:
+            try:
+                st = pli.recognize(sub_file)
+            except (TypeError, NotImplementedError):
+                continue
+            
+            if st != None:
+                subs_type.append(st)
+                
+        self.assertEqual(len(subs_type), 1, "More than one or zero")
+        self.assertEqual(subs_type[0], given_subs_type, 'Not recognize')
+    
+    def test_mpl2Recognize(self):
+        given_subs_type = 'mpl2'
+        sub_file = self.sub_mpl2
+        
+        subs_type = []
+        
+        for pli in self.plugins:
+            try:
+                st = pli.recognize(sub_file)
+            except (TypeError, NotImplementedError):
+                continue
+            
+            if st != None:
+                subs_type.append(st)
+                
+        self.assertEqual(len(subs_type), 1, "More than one or zero")
+        self.assertEqual(subs_type[0], given_subs_type, 'Not recognize')
+    
+    def test_srtRecognize(self):
+        given_subs_type = 'srt'
+        sub_file_path = self.sub_srt
+                
+        subs_type = []
+        
+        for pli in self.plugins:
+            try:
+                st = pli.recognize(sub_file_path)
+            except (TypeError, NotImplementedError):
+                continue
+            
+            if st != None:
+                subs_type.append(st)
+        
+        self.assertEqual(len(subs_type), 1, "More than one or zero")
+        self.assertEqual(subs_type[0], given_subs_type, 'Not recognize')
+    
+    def test_tmplRecognize(self):
+        given_subs_type = 'tmpl'
+        sub_file = self.sub_tmpl
+        
+        subs_type = []
+        
+        for pli in self.plugins:
+            try:
+                st = pli.recognize(sub_file)
+            except (TypeError, NotImplementedError):
+                continue
+            
+            if st != None:
+                subs_type.append(st)
+                
+        self.assertEqual(len(subs_type), 1, "More than one or zero")
+        self.assertEqual(subs_type[0], given_subs_type, 'Not recognize')
