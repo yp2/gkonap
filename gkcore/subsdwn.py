9f35c6c7ca7cc4056d1dccfe18ab28acb4d91b40
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2012-07-18 21:15:50 +0200

    główna klasa do towrzenie pluginów dla pobierania napisów

---
+++ gkcore/subsdwn.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#
+#       gkcore.subsdwn
+#       
+#       Copyright 2012 Daniel Dereziński <daniel.derezinski@gmial.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA
+
+class SubsDownload(object):
+    """
+    Base class for plugins to download subs
+    """
+    def __init__(self):
+        self.file_path = None # path to video file
+    
+    def run(self, file_path):
+        """
+        Main method for plugin run
+        @file_path - path to video file
+        dont forget to assign it to self.file_path
+        """
+        raise NotImplementedError
