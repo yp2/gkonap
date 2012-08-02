2a863877b231f36d84c051b9b65a74ce63d52cd2
Daniel Derezinski <daniel.derezinski@gmail.com>
2012-08-02 17:41:50 +0430

    Update plugins/tmpl.py

--- plugins/tmpl.py
+++ plugins/tmpl.py
@@ -37,6 +37,7 @@ class PluginTMPL(ConvertBase):
         self.re_subs_type = '^\d{1,2}:\d{2}:\d{2}:'
         self.re_decompose_subs = re.compile(r'(\d{1,2}:\d{2}:\d{2}:)')
         self.compose_line = '%s%s\n'
+        self.subs_file_ext = '.txt'
         
     def decompose(self, subtitle_file_path, movie_fps):
         super(PluginTMPL, self).decompose(subtitle_file_path, movie_fps)
