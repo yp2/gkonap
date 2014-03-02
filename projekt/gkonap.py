c9522852c3deb24ccc3bf2fdae7cc22677d343d9
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2014-03-02 12:15:21 +0100

    cos tam

--- projekt/gkonap.py
+++ projekt/gkonap.py
@@ -1,8 +1,9 @@
-class ResponseConvert (Response) :
+class ResponseConvert (Response):
+
 	'''Obiekt odpowiedzi dla pluginów convert
 '''
 	def __init__(self) :
-		self.context = convert # str
+		self.context = convert  # str
 		pass
 class WorkerConvert (Worker) :
 	'''Worker dla pluginów convert'''
@@ -11,12 +12,12 @@ class WorkerConvert (Worker) :
 class Subtitle :
 	'''Klasa określająca napisy rozłożone do standardowego formatu - poszczególne linie napisów - obiekt SubLine - znajdują się w atrybucie lines - lista zawierająca ułożne w kolejności poszczególne linie napisów w postacji Subline.'''
 	def __init__(self) :
-		self.lines = None # list
+		self.lines = None  # list
 		pass
 	def append (self, subline) :
 		""" Metoda do dodawania do atr. lines - kolejnej linii w postaci obiektu SubLine. Działanie jak append typu list.
 @subline - obiekt SubLine """
-		# returns 
+		# returns
 		pass
 class SubLine :
 	'''Obiekt będący reprezentacją poszczególnych linii napisów w postaci standardowej.
@@ -25,21 +26,21 @@ start: Decimal - czas pojawienia się napisów
 stop: Decimal - czas kiedy napisy przestają być wyświetlane
 data: str - właściwe napisy'''
 	def __init__(self) :
-		self.start = None # Decimal
-		self.stop = None # Decimal
-		self.data = None # str
+		self.start = None  # Decimal
+		self.stop = None  # Decimal
+		self.data = None  # str
 		pass
 	def set_start (self, time) :
 		""" Metoda do ustawiania atr. start na czas w formacie standardowym w postaci obiektu Decimal """
-		# returns 
+		# returns
 		pass
 	def set_stop (self, time) :
 		""" Metoda do ustawiania atr. stop na czas w formacie standardowym w postaci obiektu Decimal """
-		# returns 
+		# returns
 		pass
 	def set_data (self, data) :
 		""" Metoda do ustawiania atr. data w postraci typu str - linia napisów po przekształceniu na standardowy format. """
-		# returns 
+		# returns
 		pass
 	def get_start (self) :
 		""" Metoda pobiera wartość atr. start. """
@@ -59,13 +60,13 @@ class Worker :
 	def __init__(self) :
 		pass
 class ResponsFactory :
-	'''Fabryka obiektów Response na potrzeby różnych elemementów systemu. 
+	'''Fabryka obiektów Response na potrzeby różnych elemementów systemu.
 Zwaraca obiekty Response w zależności od kontekstu (części aplikacji).
 Przetrzymuje słownik zawierający jako klucz kontekst a wartość referencje do odpowiedniej klasy.
 @responses - słownik zwierający kontekst i odpiwednią klase response.
 Jeżeli danej odpowiedzi nie ma klasa zwraca ogólny obiekt response'''
 	def __init__(self) :
-		self.responses = None # dict
+		self.responses = None  # dict
 		pass
 	def get_respons (self, context) :
 		""" Metoda zwraca obiekt response w zależności od podanego kontekstu. Jeżeli nie ma takiego kontekstu zwraca ogólną postać odpowiedzi.  """
@@ -79,20 +80,20 @@ class PluginConvert :
 		""" Metoda do ustawiania danych potrzebnych pluginowi do dekompozycji napisów na standardowy format. Dane to wczytany plik z liniami po readlines
 
 file_lines - wczytany plik w postaci listy linii z open().readlines """
-		# returns 
+		# returns
 		pass
 	def recognize (self) :
 		""" Tu trzeba się zastanowić jak to wykonać - czy zawansowana implementacja w metodzie czy też może zwracanie jakieś klasy która to wykona ??? """
-		# returns 
+		# returns
 		pass
 	def get_decompose_subtitle (self) :
-		""" Zastanowić się co zwraca może jakiś obiekt który będzie miał dane oraz w razie błędu informacje o błedzie (wyjątek) - jeżeli tak to potrzebny jest zbiór wyjątków 
+		""" Zastanowić się co zwraca może jakiś obiekt który będzie miał dane oraz w razie błędu informacje o błedzie (wyjątek) - jeżeli tak to potrzebny jest zbiór wyjątków
 
 Zwaraca klase Subtitle zawirającą SubLine'y przekonwertowane do standardowego fotmatu """
 		# returns Response (list)
 		pass
 	def get_compose_subtitle (self) :
-		""" Zastanowić się co zwraca może jakiś obiekt który będzie miał dane oraz w razie błędu informacje o błedzie (wyjątek) - jeżeli tak to potrzebny jest zbiór wyjątków 
+		""" Zastanowić się co zwraca może jakiś obiekt który będzie miał dane oraz w razie błędu informacje o błedzie (wyjątek) - jeżeli tak to potrzebny jest zbiór wyjątków
 
 Funkcja zwaraca listę zawierającą linie po konwersji do zapisuj właściowego pliku w postaci dla open().writelines """
 		# returns Response (list)
@@ -103,7 +104,7 @@ Funkcja zwaraca listę zawierającą linie po konwersji do zapisuj właściowego
 		pass
 	def set_decompose_subtitle (self, subtitle) :
 		""" Metoda przyjumje obiekt Subtitle zawierający SubLine's - napisy rozłożone do formatu standarodwego gotowego do kompozycji. Bez tego kroku nie można przekonwertować właściwych napisów na określony przez plugin typ. """
-		# returns 
+		# returns
 		pass
 class Response :
 	'''Obiekt odpowiedzi zwracany przez pluginy. Zwracany jest on w odpowiedzi na zapytania, żadania wykonania pracy. Attr:
@@ -112,10 +113,10 @@ exceptions: list - lista wyjątków gdy praca/zadanie nie zostało wykonane nie 
 log: list - lista zawierająca ewentualny zapis pracy poszczególnych metod - nie jest to obowiązkowe
 data: typ zależny od pluginu / wykonanego zadania. Dane są obecne gdy praca się powiodła, jeżeli praca nie została wykonana nie ma żadnych danych.'''
 	def __init__(self) :
-		self.success = None # bool
-		self.execeptions = None # list
-		self.log = None # list
-		self.data = None # data
+		self.success = None  # bool
+		self.execeptions = None  # list
+		self.log = None  # list
+		self.data = None  # data
 		pass
 	def get_success (self) :
 		""" Zwaraca warotść bool powodzenia wykonania pracy """
@@ -135,31 +136,31 @@ data: typ zależny od pluginu / wykonanego zadania. Dane są obecne gdy praca si
 		pass
 	def print_log (self) :
 		""" drukuje logi """
-		# returns 
+		# returns
 		pass
 	def print_exceptions (self) :
 		""" drukuje wyjątki oraz ich opisy """
-		# returns 
+		# returns
 		pass
 	def set_success (self, state) :
 		""" ustawia succes wykonanej pracy """
-		# returns 
+		# returns
 		pass
 	def append_log (self, log_line) :
 		""" dodaje pojedyńczy log do attr log """
-		# returns 
+		# returns
 		pass
 	def set_data (self, data) :
 		""" ustawia attr data na typ zależny od pluginu oraz pracy """
-		# returns 
+		# returns
 		pass
 	def append_exception (self, exception) :
 		""" dodaje do attr exception wyjątek """
-		# returns 
+		# returns
 		pass
 class ResponseSubDownload (Response) :
 	'''Obiekt odpowiedzi dla pluginów SubDownload
 '''
 	def __init__(self) :
-		self.context = subdownload # str
+		self.context = subdownload  # str
 		pass
